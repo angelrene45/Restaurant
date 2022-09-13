0f30b32db916cd209cc31a37ed4aa044bd1bf0cb
beto <alberto.perez@globant.com>
2022-09-13 15:16:01 -0500

    hanle inputs

--- frontend/react-app/src/Components/Login/Login.js
+++ frontend/react-app/src/Components/Login/Login.js
@@ -4,9 +4,21 @@ import { useState } from 'react';
 
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
+    console.log('')
+  };
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value)
+    console.log(email)
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value)
+    console.log(password)
   };
 
   return (
@@ -15,11 +27,11 @@ const Login = () => {
       <form>
         <div>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required value={email} onChange={handleEmail}/>
         </div>
         <div>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required value={password} onChange={handlePassword}/>
         </div>
         <div>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
