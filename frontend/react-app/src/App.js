d967a10c4ebf22dbcc3b477426af6cac8656ae8a
Antonio de Jesus Rodriguez Esparza <66225707+Antonio-Rdz@users.noreply.github.com>
2022-09-13 17:54:53 -0500

    Add files via upload

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,12 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
-
-import Login from './Components/Login/Login';
+import logo from './logo.svg';
+import './App.css';
 import Layout from './Layout/Layout';
+import {NavbarAdmin, SidebarAdmin } from "./components";
+
 
 function App() {
   return (
     <Layout>
-      <Login></Login>
+      
+      
+      <div className='fixed md:static bg-sky-900 navbar w-full text-green-400 p-5 m-0'>
+        <NavbarAdmin />
+      </div>
+      <div className='w-40 fixed sidebar  bg-gray-900 shadow-lg shadow-gray-500/90 h-full text-green-300 p-5'>
+        <SidebarAdmin />
+        </div>
+      <Login />
+    
     </Layout>
     
   );
