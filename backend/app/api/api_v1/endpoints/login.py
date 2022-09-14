d42e4ede98763991409ec623a5f1c1d133bd3b05
angelrene45 <ar.herrera0@gmail.com>
2022-09-14 18:40:12 -0500

    add sent emails, reset password, templates email

--- backend/app/api/api_v1/endpoints/login.py
+++ backend/app/api/api_v1/endpoints/login.py
@@ -13,6 +13,7 @@ from app.core.security import get_password_hash
 from app.utils import (
     generate_password_reset_token,
     verify_password_reset_token,
+    send_reset_password_email
 )
 
 router = APIRouter()
@@ -53,23 +54,23 @@ def test_token(current_user: models.User = Depends(deps.get_current_user)) -> An
     return current_user
 
 
-# @router.post("/password-recovery/{email}", response_model=schemas.Msg)
-# def recover_password(email: str, db: Session = Depends(deps.get_db)) -> Any:
-#     """
-#     Password Recovery
-#     """
-#     user = crud.user.get_by_email(db, email=email)
+@router.post("/password-recovery/{email}", response_model=schemas.Msg)
+def recover_password(email: str, db: Session = Depends(deps.get_db)) -> Any:
+    """
+    Password Recovery
+    """
+    user = crud.user.get_by_email(db, email=email)
 
-#     if not user:
-#         raise HTTPException(
-#             status_code=404,
-#             detail="The user with this username does not exist in the system.",
-#         )
-#     password_reset_token = generate_password_reset_token(email=email)
-#     send_reset_password_email(
-#         email_to=user.email, email=email, token=password_reset_token
-#     )
-#     return {"msg": "Password recovery email sent"}
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="The user with this username does not exist in the system.",
+        )
+    password_reset_token = generate_password_reset_token(email=email)
+    send_reset_password_email(
+        email_to=user.email, email=email, token=password_reset_token
+    )
+    return {"msg": "Password recovery email sent"}
 
 
 @router.post("/reset-password/", response_model=schemas.Msg)
