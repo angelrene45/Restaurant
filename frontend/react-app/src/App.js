368de59bb92eb762199567efbd85f88e73704b9c
beto <alberto.perez@globant.com>
2022-10-06 13:30:30 -0500

    solved merge

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,64 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-import Layout from './Layout/Layout';
-import {NavbarAdmin, SidebarAdmin } from "./components";
-
-
-function App() {
-  return (
-    <Layout>
-      
-      
-      <div className='fixed md:static bg-sky-900 navbar w-full text-green-400 p-5 m-0'>
-        <NavbarAdmin />
-      </div>
-      <div className='w-40 fixed sidebar  bg-gray-900 shadow-lg shadow-gray-500/90 h-full text-green-300 p-5'>
-        <SidebarAdmin />
-        </div>
-      <Login />
-    
-    </Layout>
-    
-  );
-}
-
-export default App;
-=======
-import logo from './logo.svg';
-=======
->>>>>>> FrontBeto
-import './App.css';
 import { useSelector }from 'react-redux';
 import { Routes, Route } from 'react-router-dom'
-
 
 import DashAdmin from './pages/Admin/DashAdmin';
 import DashClie from './pages/Clients/DashClie';
 import DashEmp  from './pages/Employees/DashEmp';
 import Login from './Components/Login/Login'
 import Layout from './Layout/Layout';
-import {NavbarAdmin, SidebarAdmin } from "./components";
 
-
-<<<<<<< HEAD
-function App() {
-  return (
-    <Layout>
-      
-      
-      <div className='fixed md:static bg-sky-900 navbar w-full text-green-400 p-5 m-0'>
-        <NavbarAdmin />
-      </div>
-      <div className='w-40 fixed sidebar  bg-gray-900 shadow-lg shadow-gray-500/90 h-full text-green-300 p-5'>
-        <SidebarAdmin />
-        </div>
-      <Login />
-    
-    </Layout>
-    
-=======
 const App = () => {
 
   const authorization = useSelector(state => state.LoginReducer.authorization)
@@ -74,9 +22,7 @@ const App = () => {
           <Route path='*' element={ (authorization && role) === 'admin' ? <DashAdmin />  : (authorization && role !== 'admin') ? <DashEmp /> : <Login /> } />
         </Routes>
       </Layout>
->>>>>>> FrontBeto
   );
 };
 
 export default App;
->>>>>>> Stashed changes
